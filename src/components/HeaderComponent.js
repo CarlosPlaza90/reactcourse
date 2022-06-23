1f4d8583c57279e80e8ee7f6bc7b5159077276e3
CarlosPlaza <charlieplazaamado@gmail.com>
2022-06-23 12:52:28 -0500

    fixing

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,30 @@
+import React, { Component, component } from "react";
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar dark>
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                
+                    <div className="container">
+                        <div className="row row-Header">
+                            <div className="col-12 com-sm-6">
+                                <h1>Ristorante Con Fusion</h1>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                
+            </React.Fragment>
+        )
+    }
+}
+
+export default Header;
